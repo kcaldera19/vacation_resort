05347f1bff6c682c7a2d7ce81b215d103ae1faa4
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-06 14:11:46 -0500

    added variables and month

--- scripts/overnightStay.js
+++ scripts/overnightStay.js
@@ -1,29 +1,58 @@
 "use strict"
 console.log("works");
+// You price a stay using the in-season and out-of-season rates based on their check-in date. For
+// example, if someone checks in to a Queen room on 3/20 (March) the rate is $150/night, but if
+// they check in on 6/20 (June), the rate is $250/night. NOTE: The same rate is applied
+// throughout the entire stay and is solely based on check-in date.
+// In this program, you should implement a function named getRoomRate() that is passed in
+// the check-in date and room type. It will return a value like $250 or $150 that represents the
+// nightly rate for that room.
+
+
+// * getRoomRate(estimateForm.checkInDate.value,estimateForm.roomType.value);
+
 window.onload = function(){
     let estimateForm =document.querySelector("#estimateStayForm");
     estimateForm.addEventListener("submit",estimateStay);
 
 }
 function estimateStay(event){
+ 
+    
     event.preventDefault();
     let estimateForm = event.target;
 
     
 
-    let roomType = Number(estimateForm.numberOfNights.value)* getRoomRate(estimateForm.checkInDate.value,estimateForm.roomType.value);
-
+    let numberOfNights = Number(estimateForm.numberOfNights.value);
+    let CheckInDate = estimateForm.checkInDate.value;
+    let roomType = estimateForm.roomType.value;
     console.log(roomType);
     console.log("you submitted the form");
 
     console.log(estimateForm.checkInDate.value)
 }
 function getRoomRate(checkInDate,roomType){
+    let outSeasonSuite = 250;
+    let seasonSuite = 350;
+    let outSeasonQueenandKing = 150;
+    let seasonQueenAndKing = 250;
+    let tax = 12/100;
+
+    let checkInMonth = new Date(checkInDate).getMonth();
+    // june-August
+    let inSeason =[5,6,7]; 
+    
+    
+    
+    if (inSeason === )
 
     if(roomType === "suite"){
+
         return 150;
     }
     return 150
+    // if(roomType === "")
     
 
 
