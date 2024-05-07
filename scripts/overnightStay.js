5fc6a5686f2a2a081bd5d40ce8dac857b460894d
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-06 21:37:01 -0500

    changed some formulas

--- scripts/overnightStay.js
+++ scripts/overnightStay.js
@@ -4,10 +4,17 @@ console.log("works");
 // example, if someone checks in to a Queen room on 3/20 (March) the rate is $150/night, but if
 // they check in on 6/20 (June), the rate is $250/night. NOTE: The same rate is applied
 // throughout the entire stay and is solely based on check-in date.
+
+
 // In this program, you should implement a function named getRoomRate() that is passed in
 // the check-in date and room type. It will return a value like $250 or $150 that represents the
 // nightly rate for that room.
-
+// DEVELOPER's TRICK: To make pricing a room easy, start by always returning 150.00
+// from the function and get the rest of the page working. Then go back in that function
+// and put the correct logic for the specified inputs.
+// If the user has AAA or requests a senior discount, we take 10% off the room rate. If they are a
+// veteran or request military pricing, we take 20% off the room rate.
+// Taxes are 12% of the after-discount room cost.
 
 // * getRoomRate(estimateForm.checkInDate.value,estimateForm.roomType.value);
 
@@ -23,7 +30,7 @@ function estimateStay(event) {
 
     let theForm = event.target;
     let roomtype = 0;
-    let totalRoomPrice = roomtype  * Number(theForm.seasonQueenOrKing.value);
+    let totalRoomPrice = roomtype * Number(theForm.seasonQueenOrKing.value);
 
 }
 function getRoomRate(checkInDate, roomType) {
@@ -38,37 +45,45 @@ function getRoomRate(checkInDate, roomType) {
     let inSeason = [5, 6, 7];
     inSeason = inSeason.includes(checkInMonth);
 
+    let roomRate;
+    if (roomType === "suite") {
 
-    if (estimateForm.roomType.value === "suite") {
-    if (inSeason) {
-            return seasonSuite;
+        if (inSeason) {
+            roomRate = seasonSuite;
 
         } else {
-            return outSeasonSuite;
+            roomRate = outSeasonSuite;
         }
     } else if (estimateForm.roomType.value === "queen" || roomType === "king") {
         if (inSeason) {
-            return seasonQueenOrKing;
+            roomRate = seasonQueenOrKing;
 
         } else {
-            return outSeasonQueenOrKing;
+            roomRate = outSeasonQueenOrKing;
         }
+    } else {
+        roomRate = 0;
+
     }
-    
+
     // dicount
     let dicountAmount = 0;
-    if(theForm.dicount.value === "aaa"){
-        dicountAmount
-    }if(estimateForm.aaa.value.checked){
-        
+    if (theForm.aaa.value.checked === "aaa") {
+        dicountAmount = roomRate * (10 / 100);
+    } if (theForm.military.value.checked === "military") {
+        dicountAmount = roomRate * (20 / 100);
 
-    }if(estimateForm.military.checked){
+    } else {
+        return roomRate;
 
     }
-    
 
 
-    
+
+
+
+
+
 
 
 
